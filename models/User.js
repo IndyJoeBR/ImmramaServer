37b681276dcb0a3b6702a05e43913992e94e137e
IndyJoe <IndyJoeBRJS@gmail.com>
2020-11-29 15:54:01 -0500

    reinstall of a lot

--- models/User.js
+++ models/User.js
@@ -21,32 +21,3 @@ module.exports = function (sequelize, DataTypes) {
   });  
 };
 
-
-
-/*
-const { DataTypes, sequelize } = require('sequelize');
-const theDatabase = require('../db');
-
-const UserModel = sequelize.define('user', {
-  username: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    unique: true
-  },
-  passwordhash: {
-    type: DataTypes.STRING(100),
-    allowNull: false
-  },
-  userEmail: {
-    type: DataTypes.STRING(100),
-    allowNull: false
-  },
-  userAdmin: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  }
-
-}); //  End of User model
-
-module.exports = UserModel;
-*/
