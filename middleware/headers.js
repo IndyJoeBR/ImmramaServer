c54770483fda43dd80561273beef5212a9a98e47
IndyJoe <IndyJoeBRJS@gmail.com>
2020-12-02 14:17:34 -0500

    playing with headers

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,7 +1,7 @@
 const CorsMiddleware = (request, response, next) => {
-  response.header('access-control-allow-origin', '*');
-  response.header('access-control-allow-methods', 'POST, PUT, GET, DELETE, OPTIONS');
-  response.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS');
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
   return next();
 }
--- middleware/validateSession.js
+++ middleware/validateSession.js
@@ -6,7 +6,7 @@ const validateSession = (request, response, next) => {
   if (request.method == 'OPTIONS') {
     return next()
   } else {
-    const token = request.headers.authorization;
+    const token = request.headers.Authorization;
     console.log(request.headers)
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
         console.log(`INVALID TOKEN: ${decodedToken}`)
