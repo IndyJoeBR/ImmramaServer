d27d446b93df926823c354b7ccf1d540c0b0e2ed
IndyJoe <IndyJoeBRJS@gmail.com>
2020-12-02 13:39:17 -0500

    another

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,7 +1,7 @@
 const CorsMiddleware = (request, response, next) => {
   response.header('access-control-allow-origin', '*');
   response.header('access-control-allow-methods', 'POST, PUT, GET, DELETE, OPTIONS');
-  response.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  response.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
 
   return next();
 }
